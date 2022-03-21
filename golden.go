138981b7d42d00749d390fc5fd5248fd71f02b60
Jim Myhrberg <contact@jimeh.me>
2022-03-21 23:09:43 +0000

    wip: refactor Golden to a interface, improve testability of main implementation

--- golden.go
+++ golden.go
@@ -3,7 +3,7 @@
 //
 // Golden file names are based on the name of the test function and any subtest
 // names by calling t.Name(). File names are sanitized to ensure they're
-// compatible with Linux, macOS and Windows systems regardless of what crazy
+// compatible with Linux, macOS and Windows systems regardless of what
 // characters might be in a subtest's name.
 //
 // Usage
@@ -123,30 +123,37 @@
 package golden
 
 import (
-	"io/ioutil"
 	"os"
 	"path/filepath"
 	"strings"
 	"testing"
+
+	"github.com/jimeh/go-golden/sanitize"
+	"github.com/spf13/afero"
 )
 
-const (
-	DefaultDirMode  = 0o755
-	DefaultFileMode = 0o644
-	DefaultSuffix   = ".golden"
-	DefaultDirname  = "testdata"
-)
+// TestingT is a interface describing a sub-set of methods of *testing.T which
+// golden uses.
+type TestingT interface {
+	Error(args ...interface{})
+	Errorf(format string, args ...interface{})
+	FailNow()
+	Fatal(args ...interface{})
+	Fatalf(format string, args ...interface{})
+	Helper()
+	Log(args ...interface{})
+	Logf(format string, args ...interface{})
+	Name() string
+}
 
-var DefaultUpdateFunc = EnvUpdateFunc
-
-var global = New()
+var defaultGolden = New()
 
 // File returns the filename of the golden file for the given *testing.T
 // instance as determined by t.Name().
 func File(t *testing.T) string {
 	t.Helper()
 
-	return global.File(t)
+	return defaultGolden.File(t)
 }
 
 // Get returns the content of the golden file for the given *testing.T instance
@@ -155,7 +162,7 @@ func File(t *testing.T) string {
 func Get(t *testing.T) []byte {
 	t.Helper()
 
-	return global.Get(t)
+	return defaultGolden.Get(t)
 }
 
 // Set writes given data to the golden file for the given *testing.T instance as
@@ -164,7 +171,7 @@ func Get(t *testing.T) []byte {
 func Set(t *testing.T, data []byte) {
 	t.Helper()
 
-	global.Set(t, data)
+	defaultGolden.Set(t, data)
 }
 
 // FileP returns the filename of the specifically named golden file for the
@@ -172,7 +179,7 @@ func Set(t *testing.T, data []byte) {
 func FileP(t *testing.T, name string) string {
 	t.Helper()
 
-	return global.FileP(t, name)
+	return defaultGolden.FileP(t, name)
 }
 
 // GetP returns the content of the specifically named golden file belonging
@@ -184,7 +191,7 @@ func FileP(t *testing.T, name string) string {
 func GetP(t *testing.T, name string) []byte {
 	t.Helper()
 
-	return global.GetP(t, name)
+	return defaultGolden.GetP(t, name)
 }
 
 // SetP writes given data of the specifically named golden file belonging to
@@ -196,7 +203,7 @@ func GetP(t *testing.T, name string) []byte {
 func SetP(t *testing.T, name string, data []byte) {
 	t.Helper()
 
-	global.SetP(t, name, data)
+	defaultGolden.SetP(t, name, data)
 }
 
 // Update returns true when golden is set to update golden files. Should be used
@@ -206,104 +213,228 @@ func SetP(t *testing.T, name string, data []byte) {
 // environment variable is set to a truthy value. To customize create a custom
 // *Golden instance with New() and set a new UpdateFunc value.
 func Update() bool {
-	return global.Update()
+	return defaultGolden.Update()
 }
 
 // Golden handles all interactions with golden files. The top-level package
-// functions all just proxy through to a default global *Golden instance.
-type Golden struct {
-	// DirMode determines the file system permissions of any folders created to
-	// hold golden files.
-	DirMode os.FileMode
+// functions proxy through to a default global Golden instance.
+type Golden interface {
+	// File returns the filename of the golden file for the given testing.TB
+	// instance as determined by t.Name().
+	File(t TestingT) string
 
-	// FileMode determines the file system permissions of any created or updated
-	// golden files written to disk.
-	FileMode os.FileMode
+	// Get returns the content of the golden file for the given TestingT
+	// instance as determined by t.Name(). If no golden file can be found/read,
+	// it will fail the test by calling t.Fatal().
+	Get(t TestingT) []byte
 
-	// Suffix determines the filename suffix for all golden files. Typically
-	// this should be ".golden", but can be changed here if needed.
-	Suffix string
+	// Set writes given data to the golden file for the given TestingT
+	// instance as determined by t.Name(). If writing fails it will fail the
+	// test by calling t.Fatal() with error details.
+	Set(t TestingT, data []byte)
 
-	// Dirname is the name of the top-level directory at the root of the package
-	// which holds all golden files. Typically this should "testdata", but can
-	// be changed here if needed.
-	Dirname string
+	// FileP returns the filename of the specifically named golden file for the
+	// given TestingT instance as determined by t.Name().
+	FileP(t TestingT, name string) string
 
-	// UpdateFunc is used to determine if golden files should be updated or
-	// not. Its boolean return value is returned by Update().
-	UpdateFunc UpdateFunc
+	// GetP returns the content of the specifically named golden file belonging
+	// to the given TestingT instance as determined by t.Name(). If no golden
+	// file can be found/read, it will fail the test with t.Fatal().
+	//
+	// This is very similar to Get(), but it allows multiple different golden
+	// files to be used within the same one TestingT instance.
+	GetP(t TestingT, name string) []byte
+
+	// SetP writes given data of the specifically named golden file belonging to
+	// the given TestingT instance as determined by t.Name(). If writing fails
+	// it will fail the test with t.Fatal() detailing the error.
+	//
+	// This is very similar to Set(), but it allows multiple different golden
+	// files to be used within the same one TestingT instance.
+	SetP(t TestingT, name string, data []byte)
+
+	// Update returns true when golden is set to update golden files. Should be
+	// used to determine if golden.Set() or golden.SetP() should be called or
+	// not.
+	//
+	// Default behavior uses EnvUpdateFunc() to check if the "GOLDEN_UPDATE"
+	// environment variable is set to a truthy value. To customize set a new
+	// UpdateFunc value on *Golden.
+	Update() bool
 }
 
-// New returns a new *Golden instance with default values correctly
-// populated. This is ideally how you should create a custom *Golden, and then
-// modify the relevant fields as you see fit.
-func New() *Golden {
-	return &Golden{
-		DirMode:    DefaultDirMode,
-		FileMode:   DefaultFileMode,
-		Suffix:     DefaultSuffix,
-		Dirname:    DefaultDirname,
-		UpdateFunc: DefaultUpdateFunc,
+// New returns a new Golden instance. Used to create custom Golden instances.
+// See the the various Option functions for details of what can be customized.
+func New(options ...Option) Golden {
+	g := &golden{
+		dirMode:    0o755,
+		fileMode:   0o644,
+		suffix:     ".golden",
+		dirname:    "testdata",
+		updateFunc: EnvUpdateFunc,
+		fs:         afero.NewOsFs(),
+		logOnWrite: true,
 	}
+
+	for _, opt := range options {
+		opt.apply(g)
+	}
+
+	return g
 }
 
-// File returns the filename of the golden file for the given *testing.T
-// instance as determined by t.Name().
-func (s *Golden) File(t *testing.T) string {
+type Option interface {
+	apply(*golden)
+}
+
+type optionFunc func(*golden)
+
+func (fn optionFunc) apply(g *golden) {
+	fn(g)
+}
+
+// WithDirMode sets the file system permissions used for any folders created to
+// hold golden files.
+//
+// When this option is not provided, the default value is 0o755.
+func WithDirMode(mode os.FileMode) Option {
+	return optionFunc(func(g *golden) {
+		g.dirMode = mode
+	})
+}
+
+// WithFileMode sets the file system permissions used for any created or updated
+// golden files written to.
+//
+// When this option is not provided, the default value is 0o644.
+func WithFileMode(mode os.FileMode) Option {
+	return optionFunc(func(g *golden) {
+		g.fileMode = mode
+	})
+}
+
+// WithSuffix sets the filename suffix used for all golden files.
+//
+// When this option is not provided, the default value is ".golden".
+func WithSuffix(suffix string) Option {
+	return optionFunc(func(g *golden) {
+		g.suffix = suffix
+	})
+}
+
+// WithDirname sets the name of the top-level directory used to hold golden
+// files.
+//
+// When this option is not provided, the default value is "testdata".
+func WithDirname(name string) Option {
+	return optionFunc(func(g *golden) {
+		g.dirname = name
+	})
+}
+
+// WithUpdateFunc sets the function used to determine if golden files should be
+// updated or not. Essentially the provided UpdateFunc is called by Update().
+//
+// When this option is not provided, the default value is EnvUpdateFunc.
+func WithUpdateFunc(fn UpdateFunc) Option {
+	return optionFunc(func(g *golden) {
+		g.updateFunc = fn
+	})
+}
+
+// WithFs sets s afero.Fs instance which is used to read/write all golden files.
+//
+// When this option is not provided, the default value is afero.NewOsFs().
+func WithFs(fs afero.Fs) Option {
+	return optionFunc(func(g *golden) {
+		g.fs = fs
+	})
+}
+
+// WithSilentWrites silences the "golden: writing [...]" log messages whenever
+// set functions write a golden file to disk.
+func WithSilentWrites() Option {
+	return optionFunc(func(g *golden) {
+		g.logOnWrite = false
+	})
+}
+
+// golden is the underlying struct that implements the Golden interface.
+type golden struct {
+	// dirMode determines the file system permissions of any folders created to
+	// hold golden files.
+	dirMode os.FileMode
+
+	// fileMode determines the file system permissions of any created or updated
+	// golden files written to disk.
+	fileMode os.FileMode
+
+	// suffix determines the filename suffix for all golden files. Typically
+	// this should be ".golden", but can be changed here if needed.
+	suffix string
+
+	// dirname is the name of the top-level directory at the root of the package
+	// which holds all golden files. Typically this should be "testdata", but
+	// can be changed here if needed.
+	dirname string
+
+	// updateFunc is used to determine if golden files should be updated or
+	// not. Its boolean return value is returned by Update().
+	updateFunc UpdateFunc
+
+	// fs is used for all file system operations. This enables providing custom
+	// afero.fs instances which can be useful for testing purposes.
+	fs afero.Fs
+
+	// logOnWrite determines if a message is logged with t.Logf when a golden
+	// file is written to with either of the set methods.
+	logOnWrite bool
+}
+
+// Ensure golden satisfies Golden interface.
+var _ Golden = &golden{}
+
+func (s *golden) File(t TestingT) string {
+	t.Helper()
+
 	return s.file(t, "")
 }
 
-// Get returns the content of the golden file for the given *testing.T instance
-// as determined by t.Name(). If no golden file can be found/read, it will fail
-// the test by calling t.Fatal().
-func (s *Golden) Get(t *testing.T) []byte {
+func (s *golden) Get(t TestingT) []byte {
+	t.Helper()
+
 	return s.get(t, "")
 }
 
-// Set writes given data to the golden file for the given *testing.T instance as
-// determined by t.Name(). If writing fails it will fail the test by calling
-// t.Fatal() with error details.
-func (s *Golden) Set(t *testing.T, data []byte) {
+func (s *golden) Set(t TestingT, data []byte) {
+	t.Helper()
+
 	s.set(t, "", data)
 }
 
-// FileP returns the filename of the specifically named golden file for the
-// given *testing.T instance as determined by t.Name().
-func (s *Golden) FileP(t *testing.T, name string) string {
-	if name == "" {
-		if t != nil {
-			t.Fatal("golden: name cannot be empty")
-		}
+func (s *golden) FileP(t TestingT, name string) string {
+	t.Helper()
 
-		return ""
+	if name == "" {
+		t.Fatalf("golden: test name cannot be empty")
 	}
 
 	return s.file(t, name)
 }
 
-// GetP returns the content of the specifically named golden file belonging
-// to the given *testing.T instance as determined by t.Name(). If no golden file
-// can be found/read, it will fail the test with t.Fatal().
-//
-// This is very similar to Get(), but it allows multiple different golden files
-// to be used within the same one *testing.T instance.
-func (s *Golden) GetP(t *testing.T, name string) []byte {
+func (s *golden) GetP(t TestingT, name string) []byte {
+	t.Helper()
+
 	if name == "" {
 		t.Fatal("golden: name cannot be empty")
-
-		return nil
 	}
 
 	return s.get(t, name)
 }
 
-// SetP writes given data of the specifically named golden file belonging to
-// the given *testing.T instance as determined by t.Name(). If writing fails it
-// will fail the test with t.Fatal() detailing the error.
-//
-// This is very similar to Set(), but it allows multiple different golden files
-// to be used within the same one *testing.T instance.
-func (s *Golden) SetP(t *testing.T, name string, data []byte) {
+func (s *golden) SetP(t TestingT, name string, data []byte) {
+	t.Helper()
+
 	if name == "" {
 		t.Fatal("golden: name cannot be empty")
 	}
@@ -311,65 +442,65 @@ func (s *Golden) SetP(t *testing.T, name string, data []byte) {
 	s.set(t, name, data)
 }
 
-func (s *Golden) file(t *testing.T, name string) string {
-	if t.Name() == "" {
-		t.Fatalf("golden: could not determine filename for: %+v", t)
+func (s *golden) file(t TestingT, name string) string {
+	t.Helper()
 
-		return ""
+	if t.Name() == "" {
+		t.Fatalf(
+			"golden: could not determine filename for given %T instance", t,
+		)
 	}
 
-	base := []string{s.Dirname, filepath.FromSlash(t.Name())}
+	base := []string{s.dirname, filepath.FromSlash(t.Name())}
 	if name != "" {
 		base = append(base, name)
 	}
 
-	f := filepath.Clean(filepath.Join(base...) + s.Suffix)
+	f := filepath.Clean(filepath.Join(base...) + s.suffix)
 
 	dirty := strings.Split(f, string(os.PathSeparator))
 	clean := make([]string, 0, len(dirty))
 	for _, s := range dirty {
-		clean = append(clean, sanitizeFilename(s))
+		clean = append(clean, sanitize.Filename(s))
 	}
 
 	return strings.Join(clean, string(os.PathSeparator))
 }
 
-func (s *Golden) get(t *testing.T, name string) []byte {
+func (s *golden) get(t TestingT, name string) []byte {
+	t.Helper()
+
 	f := s.file(t, name)
 
-	b, err := ioutil.ReadFile(f)
+	b, err := afero.ReadFile(s.fs, f)
 	if err != nil {
-		t.Fatalf("golden: failed reading %s: %s", f, err.Error())
+		t.Fatalf("golden: %s", err.Error())
 	}
 
 	return b
 }
 
-func (s *Golden) set(t *testing.T, name string, data []byte) {
+func (s *golden) set(t TestingT, name string, data []byte) {
+	t.Helper()
+
 	f := s.file(t, name)
 	dir := filepath.Dir(f)
 
-	t.Logf("golden: writing .golden file: %s", f)
-
-	err := os.MkdirAll(dir, s.DirMode)
-	if err != nil {
-		t.Fatalf("golden: failed to create directory: %s", err.Error())
-
-		return
+	if s.logOnWrite {
+		t.Logf("golden: writing golden file: %s", f)
 	}
 
-	err = ioutil.WriteFile(f, data, s.FileMode)
+	err := s.fs.MkdirAll(dir, s.dirMode)
+	if err != nil {
+		t.Fatalf("golden: failed to create directory: %s", err.Error())
+	}
+
+	err = afero.WriteFile(s.fs, f, data, s.fileMode)
 	if err != nil {
 		t.Fatalf("golden: filed to write file: %s", err.Error())
 	}
 }
 
-// Update returns true when golden is set to update golden files. Should be used
-// to determine if golden.Set() or golden.SetP() should be called or not.
-//
-// Default behavior uses EnvUpdateFunc() to check if the "GOLDEN_UPDATE"
-// environment variable is set to a truthy value. To customize set a new
-// UpdateFunc value on *Golden.
-func (s *Golden) Update() bool {
-	return s.UpdateFunc()
+func (s *golden) Update() bool {
+	return s.updateFunc()
 }
